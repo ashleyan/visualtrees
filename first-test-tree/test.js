eaa41fd596f40df8a7abf1929fa97aa6fd02cab4
Ashley An <ashley.an@columbia.edu>
2017-09-17 00:24:55 -0400

    change javascript file of tree example to json

--- first-test-tree/test.js
+++ first-test-tree/test.js
@@ -1,4 +1,5 @@
-var config = {
+var chart_config = {
+    chart: {
         container: "#basic-example",
         
         connectors: {
@@ -8,203 +9,80 @@ var config = {
             HTMLclass: 'nodeExample1'
         }
     },
-    ceo = {
+    nodeStructure: {
         text: {
             name: "Mark Hill",
             title: "Chief executive officer",
             contact: "Tel: 01 213 123 134",
         },
-    },
-
-    cto = {
-        parent: ceo,
-        text:{
-            name: "Joe Linux",
-            title: "Chief Technology Officer",
-        },
-        stackChildren: true,
-    },
-    cbo = {
-        parent: ceo,
-        stackChildren: true,
-        text:{
-            name: "Linda May",
-            title: "Chief Business Officer",
-        },
-    },
-    cdo = {
-        parent: ceo,
-        text:{
-            name: "John Green",
-            title: "Chief accounting officer",
-            contact: "Tel: 01 213 123 134",
-        },
-    },
-    cio = {
-        parent: cto,
-        text:{
-            name: "Ron Blomquist",
-            title: "Chief Information Security Officer"
-        },
-    },
-    ciso = {
-        parent: cto,
-        text:{
-            name: "Michael Rubin",
-            title: "Chief Innovation Officer",
-            contact: {val: "we@aregreat.com", href: "mailto:we@aregreat.com"}
-        },
-    },
-    cio2 = {
-        parent: cdo,
-        text:{
-            name: "Erica Reel",
-            title: "Chief Customer Officer"
-        },
-        link: {
-            href: "http://www.google.com"
-        },
-    },
-    ciso2 = {
-        parent: cbo,
-        text:{
-            name: "Alice Lopez",
-            title: "Chief Communications Officer"
-        },
-    },
-    ciso3 = {
-        parent: cbo,
-        text:{
-            name: "Mary Johnson",
-            title: "Chief Brand Officer"
-        },
-    },
-    ciso4 = {
-        parent: cbo,
-        text:{
-            name: "Kirk Douglas",
-            title: "Chief Business Development Officer"
-        },
-    }
-
-    chart_config = [
-        config,
-        ceo,
-        cto,
-        cbo,
-        cdo,
-        cio,
-        ciso,
-        cio2,
-        ciso2,
-        ciso3,
-        ciso4
-    ];
-
-
-
-
-    // Antoher approach, same result
-    // JSON approach
-
-/*
-    var chart_config = {
-        chart: {
-            container: "#basic-example",
-            
-            connectors: {
-                type: 'step'
+        children: [
+            {
+                text:{
+                    name: "Joe Linux",
+                    title: "Chief Technology Officer",
+                },
+                stackChildren: true,
+                children: [
+                    {
+                        text:{
+                            name: "Ron Blomquist",
+                            title: "Chief Information Security Officer"
+                        },
+                    },
+                    {
+                        text:{
+                            name: "Michael Rubin",
+                            title: "Chief Innovation Officer",
+                            contact: "we@aregreat.com"
+                        },
+                    }
+                ]
             },
-            node: {
-                HTMLclass: 'nodeExample1'
+            {
+                stackChildren: true,
+                text:{
+                    name: "Linda May",
+                    title: "Chief Business Officer",
+                },
+                children: [
+                    {
+                        text:{
+                            name: "Alice Lopez",
+                            title: "Chief Communications Officer"
+                        },
+                    },
+                    {
+                        text:{
+                            name: "Mary Johnson",
+                            title: "Chief Brand Officer"
+                        },
+                    },
+                    {
+                        text:{
+                            name: "Kirk Douglas",
+                            title: "Chief Business Development Officer"
+                        },
+                    }
+                ]
+            },
+            {
+                text:{
+                    name: "John Green",
+                    title: "Chief accounting officer",
+                    contact: "Tel: 01 213 123 134",
+                },
+                children: [
+                    {
+                        text:{
+                            name: "Erica Reel",
+                            title: "Chief Customer Officer"
+                        },
+                        link: {
+                            href: "http://www.google.com"
+                        },
+                    }
+                ]
             }
-        },
-        nodeStructure: {
-            text: {
-                name: "Mark Hill",
-                title: "Chief executive officer",
-                contact: "Tel: 01 213 123 134",
-            },
-            image: "../headshots/2.jpg",
-            children: [
-                {
-                    text:{
-                        name: "Joe Linux",
-                        title: "Chief Technology Officer",
-                    },
-                    stackChildren: true,
-                    image: "../headshots/1.jpg",
-                    children: [
-                        {
-                            text:{
-                                name: "Ron Blomquist",
-                                title: "Chief Information Security Officer"
-                            },
-                            image: "../headshots/8.jpg"
-                        },
-                        {
-                            text:{
-                                name: "Michael Rubin",
-                                title: "Chief Innovation Officer",
-                                contact: "we@aregreat.com"
-                            },
-                            image: "../headshots/9.jpg"
-                        }
-                    ]
-                },
-                {
-                    stackChildren: true,
-                    text:{
-                        name: "Linda May",
-                        title: "Chief Business Officer",
-                    },
-                    image: "../headshots/5.jpg",
-                    children: [
-                        {
-                            text:{
-                                name: "Alice Lopez",
-                                title: "Chief Communications Officer"
-                            },
-                            image: "../headshots/7.jpg"
-                        },
-                        {
-                            text:{
-                                name: "Mary Johnson",
-                                title: "Chief Brand Officer"
-                            },
-                            image: "../headshots/4.jpg"
-                        },
-                        {
-                            text:{
-                                name: "Kirk Douglas",
-                                title: "Chief Business Development Officer"
-                            },
-                            image: "../headshots/11.jpg"
-                        }
-                    ]
-                },
-                {
-                    text:{
-                        name: "John Green",
-                        title: "Chief accounting officer",
-                        contact: "Tel: 01 213 123 134",
-                    },
-                    image: "../headshots/6.jpg",
-                    children: [
-                        {
-                            text:{
-                                name: "Erica Reel",
-                                title: "Chief Customer Officer"
-                            },
-                            link: {
-                                href: "http://www.google.com"
-                            },
-                            image: "../headshots/10.jpg"
-                        }
-                    ]
-                }
-            ]
-        }
-    };
-
-*/
+        ]
+    }
+};
