576719279e53415fc82e6f5f7982099757396b7b
Ashley An <ashley.an@columbia.edu>
2017-09-27 17:32:16 -0400

    create new parse function that is based on function inputs

--- user-create-tree/code-input/test.js
+++ user-create-tree/code-input/test.js
@@ -10,54 +10,59 @@ function parse(x) {
     var array = [];
 
     // if no children, create array with empty string
-    if(x == "()"){
+    var j = 0;
+    for(var i = 0; i < x.length; i++) {
+        if(x[i].indexOf("(") == -1) {
+            j++;
+        }
+        if(x[i] == "("){
+            break;
+        }
+    }
+    
+    if(x.indexOf("(") > -1) {
+        array.push(x.substring(0,j));
+    }
+    else {
         array.push("");
         return array;
     }
     
     // start from first parenthesis of all children
-    var index1 = 1;
-    var index2 = 2;
+    var index1 = j;
+    var index2 = j + 1;
     // check until last parenthesis of all children
     while(index2 < x.length){
-        // when the child has more of its own children
-        if(x[index1] == x[index2]) {
-            var right = 0;
-            var left = 0;
-            if (x[index1] == "(") {
+        var right = 0;
+        var left = 0;
+        if (x[index1] == "(") {
+            left+=1;
+        }
+        if (x[index1] == ")") {
+            right+=1;
+        }
+        // create correct child
+        while(right != left && index2 < x.length) {
+            if(x[index2] == "(") {
                 left+=1;
             }
-            if (x[index1] == ")") {
+            if(x[index2] == ")") {
                 right+=1;
             }
-            // create correct child
-            while(right != left) {
-                if(x[index2] == "(") {
-                    left+=1;
-                    index2+=1;
-                }
-                if(x[index2] == ")") {
-                    right+=1;
-                    index2+=1;
-                }
-            }
-            // add to array of children
-            array.push(x.substring(index1,index2));
-            // start again to add next child to array
-            index1 = index2;
             index2+=1;
         }
-        else{
-            // when the child has none of its own children
-            array.push(x.substring(index1,index2+1));
-            index1 = index2 + 1;
-            index2 = index1 + 1;
-        }
+        // add to array of children
+        array.push(x.substring(index1,index2));
+        // start again to add next child to array
+        index1 = index2;
+        index2+=1;
     }
 
     return array;
 
 }
+
+console.log(parse(process.argv[2]));
 
 // createTree(x)
 // 1. take in x (which is a string)
