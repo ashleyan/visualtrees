5b03678059117164db6255aa64b5a6de8c08c5da
Ashley An <ashley.an@columbia.edu>
2017-10-13 13:32:11 -0400

    create server/client interaction for creating tree

--- server-client-tree/web_server.js
+++ server-client-tree/web_server.js
@@ -2,6 +2,7 @@
 var http = require('http');
 var io = require('socket.io');
 var port = 1234;
+//var z; //************ CHECK ****************//
 
 // Start the server at port 1234
 var server = http.createServer();
@@ -11,6 +12,8 @@ server.listen(port);
 // Create a Socket.IO instance, passing it our server
 var socket = io.listen(server);
 
+const { exec } = require('child_process');
+
 // Add a connect listener
 socket.on('connection', function(client){ 
     console.log('Connection to client established');
@@ -18,6 +21,21 @@ socket.on('connection', function(client){
     // Success!  Now listen to messages to be received
     client.on('message',function(event){ 
         console.log('Received message from client!',event);
+        
+        var command = "../prob-seq -i _ --stb-output _";
+        var x = exec(command, (error, stdout, stderr) => {
+            if (error) {
+              console.error(`exec error: ${error}`);
+              return;
+            }
+            console.log(`stdout: ${stdout}`);
+            // send message to client
+            socket.emit('message', stdout);
+            console.log(`stderr: ${stderr}`);
+        });
+        x.stdin.write(event);
+        x.stdin.end();
+
     });
 
     client.on('disconnect',function(){
@@ -31,11 +49,15 @@ console.log('Server running at http://127.0.0.1:' + port + '/');
 
 
 
+
+/*
 // can make 'cat' and 'hi' represented by variables
 // then use them in the above functions to allow client to use
 const { exec } = require('child_process');
+var y = 'cat';
+var w = 'hello';
 // anonymous function: doesn't have a name ; only using it once
-var x = exec('cat', (error, stdout, stderr) => {
+var x = exec(y, (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
       return;
@@ -43,6 +65,6 @@ var x = exec('cat', (error, stdout, stderr) => {
     console.log(`stdout: ${stdout}`);
     console.log(`stderr: ${stderr}`);
 });
-x.stdin.write('hi');
+x.stdin.write(w);
 x.stdin.end();
-
+*/
