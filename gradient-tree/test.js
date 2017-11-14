1aebbc993947477fc39d2d47681f112f5c4a91fc
Ashley An <ashley.an@columbia.edu>
2017-11-14 16:05:34 -0500

    create HTMLclass ids for each individual node

---
+++ gradient-tree/test.js
@@ -0,0 +1,181 @@
+// Parsing function that takes in a string
+// x can only be a single parent -> ex: "()" and "(()())" are allowed (one parent) but ()()" is not allowed (two parents)
+function parse(x) {
+    
+    // array of children
+    var array = [];
+
+    // if no children, create array with empty string
+    var j = 0;
+    for(var i = 0; i < x.length; i++) {
+        if(x[i].indexOf("(") == -1) {
+            j++;
+        }
+        if(x[i] == "("){
+            break;
+        }
+    }
+    
+    if(x.indexOf("(") > -1) {
+        array.push(x.substring(0,j).trim());
+    }
+    else {
+        array.push(x);
+        return array;
+    }
+    
+    // start from first parenthesis of all children
+    var index1 = j;
+    var index2 = j + 1;
+
+    // check until last parenthesis of all children
+    while(index2 < x.length){
+        var right = 0;
+        var left = 0;
+        if (x[index1] == "(") {
+            left+=1;
+        }
+        if (x[index1] == ")") {
+            right+=1;
+        }
+        // create correct child
+        while(right != left && index2 < x.length) {
+            if(x[index2] == "(") {
+                left+=1;
+            }
+            if(x[index2] == ")") {
+                right+=1;
+            }
+            index2+=1;
+        }
+        // add to array of children
+        array.push(x.substring(index1,index2));
+        // start again to add next child to array
+        index1 = index2;
+        index2+=1;
+    }
+
+    return array.filter((s) => s != " ");
+
+}
+
+// Takes in array of integers specifying the path, then changes the color of those nodes
+// Input is node structure
+function changePathColor(x, arr) {
+
+    if(arr.length == 0) {
+        x.HTMLclass = 'new';
+    }
+    else{
+        x.HTMLclass = 'new';
+        changePathColor(x.children[arr[0]], arr.slice(1));
+    }
+
+    return x;
+
+}
+
+// Display tree
+function displayTree(x) {
+
+    var structure = x;
+    var chart_config = {
+        chart: {
+            container: "#basic-example",
+            
+            connectors: {
+                type: 'step'
+            }
+        },
+        nodeStructure:
+            createTree(structure, "0"),
+    };
+
+    return chart_config;
+}
+
+// Highlight path in tree
+// Input should be chart config
+function highlightPath(x, path) {
+
+    var newTree = JSON.parse(JSON.stringify(x));
+    newTree.nodeStructure = setGradients( newTree.nodeStructure, path, 0.5 );
+
+    return newTree;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// x = string to create tree
+// name = id_name for node
+function createTree(x, name) {
+
+    var id_name = name;
+    var new_id_name;
+    var array = [];
+    array = parse(x);
+    var parent_name = array[0];
+
+    array = array.slice(1, array.length);
+    array = array.map((s) => s.slice(1, s.length - 1));
+
+    if (array.length == 0) {
+        return  {
+                        HTMLclass: id_name,
+                        text: {
+                            name: parent_name
+                        },
+                        children: [],
+                        weight: 0
+                }
+    }
+    else {
+        var children_array = [];
+        for(var i = 0; i < array.length; i++) {
+            new_id_name = id_name + String(i);
+            children_array.push( createTree(array[i], new_id_name) );
+        }
+
+        return {
+            HTMLclass: id_name,
+            text: {
+                name: parent_name
+            },
+            children: children_array,
+            weight: 0
+        }
+    }
+
+}
+
+// Separate function that set total value of weight
+// arr = path
+// w = color gradient
+// new value each node in tree has: totalWeight = total value of weight
+// directly modifies weight value of tree
+function setGradients(tree, arr, w) {
+    if(arr.length == 0) {
+        tree.weight = w;
+    }
+    else{
+        tree.weight = w;
+        setGradients(tree.children[arr[0]], arr.slice(1), w);
+    }
+
+    return tree;
+}
+
+// another function that uses getbyId to set the color for each node
+// directly changes color of tree
+function changeGradients(tree) {
+
+}
---
+++ gradient-tree/web_server.js
@@ -0,0 +1,85 @@
+// Require HTTP module (to start server) and Socket.IO
+var http = require('http');
+var io = require('socket.io');
+var port = 1234;
+
+// Start the server at port 1234
+var server = http.createServer();
+
+// Wait for a connection
+server.listen(port);
+
+// Create a Socket.IO instance, passing it our server
+var socket = io.listen(server);
+
+// To allow for use of command line
+const { exec } = require('child_process');
+
+// Add a connect listener
+socket.on('connection', function(client){ 
+    console.log('Connection to client established');
+
+    // Receive code from client to create tree
+    client.on('code',function(event){ 
+        console.log('Received message from client!', event);
+        
+        var command = "../prob-seq -i _ --stb-output _";
+        var x = exec(command, (error, stdout, stderr) => {
+            if (error) {
+              console.error(`exec error: ${error}`);
+              socket.emit('error', stderr);
+              return;
+            }
+            console.log(`stdout: ${stdout}`);
+            socket.emit('tree', stdout);
+            console.log(`stderr: ${stderr}`);
+        });
+        x.stdin.write(event);
+        x.stdin.end();
+    });
+
+    // Receive code from client to do sampling
+    client.on('samples',function(event){
+      console.log('Received message from client!', event);
+        
+        var command = "../prob-seq -i _ --sample 5";
+        var x = exec(command, (error, stdout, stderr) => {
+            if (error) {
+              console.error(`exec error: ${error}`);
+              socket.emit('error', stderr);
+              return;
+            }
+            console.log(`stdout: ${stdout}`);
+            socket.emit('samples', stdout);
+            console.log(`stderr: ${stderr}`);
+        });
+        x.stdin.write(event);
+        x.stdin.end();
+    });
+
+    // Receive code from client to highlight path in tree
+    client.on('path',function(event){ 
+        console.log('Received message from client!', event);
+        
+        var command = "../prob-seq -i _ --stb-output _";
+        var x = exec(command, (error, stdout, stderr) => {
+            if (error) {
+              console.error(`exec error: ${error}`);
+              socket.emit('error', stderr);
+              return;
+            }
+            console.log(`stdout: ${stdout}`);
+            socket.emit('path', stdout);
+            console.log(`stderr: ${stderr}`);
+        });
+        x.stdin.write(event);
+        x.stdin.end();
+    });
+
+    // Disconnect
+    client.on('disconnect',function(){
+        console.log('Server has disconnected');
+    });
+});
+
+console.log('Server running at http://127.0.0.1:' + port + '/');
